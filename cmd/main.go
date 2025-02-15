cbf3be1ddc842b14a35f687ec5850ecb6437aece
Nurtore Orazbek <nurtoreorazbek@Nurtores-MacBook-Air.local>
2025-02-15 23:42:41 +0500

    added dependency injections, by creating service, repository logics

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,17 @@ import (
 
 	"github.com/Nurt0re/todo-app"
 	"github.com/Nurt0re/todo-app/pkg/handler"
+	"github.com/Nurt0re/todo-app/pkg/repository"
+	"github.com/Nurt0re/todo-app/pkg/service"
 )
 
 func main() {
-	handler:= new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err:=srv.Run("8080", handler.InitRoutes()); err!=nil{
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running the server: %s", err.Error())
 	}
 
