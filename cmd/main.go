73f474dd15683f3c204e547bd4a9bc0ad83a1eb8
Nurtore Orazbek <nurtoreorazbek@Nurtores-MacBook-Air.local>
2025-02-20 23:49:57 +0500

    added postgres db to the program

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,50 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/Nurt0re/todo-app"
 	"github.com/Nurt0re/todo-app/pkg/handler"
 	"github.com/Nurt0re/todo-app/pkg/repository"
 	"github.com/Nurt0re/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err:=godotenv.Load(); err!=nil{
+		log.Fatalf("error loading environmental variables %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("error while initializing database - %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running the server: %s", err.Error())
 	}
 
+}
+
+// Нужно чтобы не хардкодить порт
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
